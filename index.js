e38d511b958907d5a22fc077ce3cf0f7ff7c780c
jyddys1 <justus.sydanmaa@hotmail.com>
2023-02-18 15:27:19 +0200

    puhelinluettelon backend step1 done

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log("Hello world!")
+const express = require('express')
+const app = express()
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456"
+  },
+  {
+    id: 2,
+    name: "Ada Lovelance",
+    number: "39-44-5323523"
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-321321321"
+  },
+  {
+    id: 4,
+    name: "Mary Poppendick",
+    number: "39-23-64654326"
+  },
+]
+
+app.get('/', (req, res) => {
+  res.send('<h1>Welcome to this page!</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
